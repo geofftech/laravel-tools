cc315baf42c51f63e30fb7c155fa8f64600af3ea
Geoff <geoff@geofftech.com>
2024-05-30 18:10:47 +1000

    add enumIn

---
+++ src/helpers.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!function_exists('enumIn')) {
+  function enumIn($check, array $enums)
+  {
+    if (!$check) {
+      return false;
+    }
+
+    foreach ($enums as $e) {
+      if ($check === $e || $check === $e->value) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
